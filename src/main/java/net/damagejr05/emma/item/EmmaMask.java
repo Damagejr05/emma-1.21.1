926c143dcfaba810d5d8f063128d0eed772e2490
Damagejr05 <Ethan.camidge05@gmail.com>
2026-01-09 15:15:50 -0500

    Added invis onEquip and broken remove invis onUnequip

--- src/main/java/net/damagejr05/emma/item/EmmaMask.java
+++ src/main/java/net/damagejr05/emma/item/EmmaMask.java
@@ -1,11 +1,15 @@
 package net.damagejr05.emma.item;
 
 
+import dev.emi.trinkets.api.SlotReference;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import dev.emi.trinkets.api.TrinketItem;
 import dev.emi.trinkets.api.TrinketsApi;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.EntityStatusEffectS2CPacket;
 
 
 public class EmmaMask extends TrinketItem {
@@ -13,20 +17,32 @@ public class EmmaMask extends TrinketItem {
         super(settings);
     }
 
+
+
+
+    @Override
+    public void onEquip(ItemStack stack, SlotReference slot, LivingEntity entity) {
+        super.onEquip(stack, slot, entity);
+
+        if (!entity.getWorld().isClient && entity instanceof PlayerEntity player){
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, Integer.MAX_VALUE, 0, false, true, false));
+        }
+    }
+
+    @Override
+    public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity){
+        super.onUnequip(stack, slot, entity);
+
+        if (!entity.getWorld().isClient && entity instanceof PlayerEntity player) {
+            player.removeStatusEffect(StatusEffects.INVISIBILITY);
+        }    }
+
+
     public static boolean isMaskEquipped(PlayerEntity player) {
         return TrinketsApi.getTrinketComponent(player).map(component -> component.isEquipped(ModItems.EMMA_MASK)).orElse(false);
     }
 
-    public void maskInvsibility(PlayerEntity player){
-        if (!player.getWorld().isClient() && isMaskEquipped(player)){
-            player.addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, 210, 0, false,true, false));
-
-
-
-        }
-    }
-
-    }
+}
 
 
 
