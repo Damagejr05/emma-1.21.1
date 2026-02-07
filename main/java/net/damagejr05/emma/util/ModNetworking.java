55dc7576619e48865c9f50275d8e450a1e4b0fa2
BrandRobKus <brandrobkus@gmail.com>
2026-02-06 21:35:44 -0500

    moved everything proper

--- main/java/net/damagejr05/emma/util/ModNetworking.java
+++
@@ -1,8 +0,0 @@
-package net.damagejr05.emma.util;
-
-import net.damagejr05.emma.Emma;
-import net.minecraft.util.Identifier;
-
-public class ModNetworking {
-    public static final Identifier LIFETIME_SYNC = Identifier.of(Emma.MOD_ID, "super_sync");
-}
--- src/main/java/net/damagejr05/emma/Emma.java
+++ src/main/java/net/damagejr05/emma/Emma.java
@@ -1,10 +1,16 @@
 package net.damagejr05.emma;
 
-import dev.emi.trinkets.api.TrinketEnums;
 import dev.emi.trinkets.api.event.TrinketDropCallback;
+import net.damagejr05.emma.entity.ModEntities;
+import net.damagejr05.emma.item.tracker.ShadowstepTracker;
+import net.damagejr05.emma.entity.TeleportPositionEntity;
+import net.damagejr05.emma.item.ModItemGroups;
 import net.damagejr05.emma.item.ModItems;
+import net.damagejr05.emma.util.ModSounds;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +22,9 @@ public class Emma implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModItemGroups.registerItemGroups();
+		ModEntities.registerModEntities();
+		ModSounds.registerSounds();
 		TrinketDropCallback.EVENT.register((dropRule, itemStack, slotReference, livingEntity) -> {
 			Item item = itemStack.getItem();
 			if (item == ModItems.EMMA_MASK) {
@@ -23,5 +32,16 @@ public class Emma implements ModInitializer {
 			}
             return dropRule;
         });
+
+		ServerLivingEntityEvents.AFTER_DAMAGE.register(
+				(entity, world, source, amount, blocked) -> {
+					if (!(entity instanceof PlayerEntity player)) return;
+
+					TeleportPositionEntity anchor = ShadowstepTracker.get(player);
+					if (anchor != null && !anchor.isRemoved()) {
+						anchor.reduceLifetimeByDamage(amount);
+					}
+				}
+		);
 	}
 }
